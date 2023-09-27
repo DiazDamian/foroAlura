7600704804fe54fdc4483b4f1a6fbbb833b8cfbf
DiazDamian <130494732+DiazDamian@users.noreply.github.com>
2023-09-27 19:19:13 -0300

    topico create tables

--- src/main/java/com/alura/controller/TopicoController.java
+++ src/main/java/com/alura/controller/TopicoController.java
@@ -1,10 +1,15 @@
 package com.alura.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alura.modelo.Topico;
 import com.alura.topico.DatosRegistroTopico;
 import com.alura.topico.TopicoRepository;
 
@@ -19,5 +24,10 @@ public class TopicoController {
 	@PostMapping
 	public void post(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico) {
 		
+	}
+	
+	@GetMapping("/{id}")
+	public List<Topico> listaDeTopicos(@PathVariable Long id){
+		return topicoRepository.findAll();
 	}
 }
